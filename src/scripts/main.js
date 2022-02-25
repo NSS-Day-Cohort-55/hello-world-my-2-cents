42eae47ae390afda2b9d1c6e33bf423284547c70
jandershock <jordan.andershock@gmail.com>
2022-02-25 09:10:05 -0600

    Updated header function Updated footer function Updated Croatia title Added some comment sections on pages

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,13 @@
 import { createFooterText } from "./footer.js";
 import * as nav from "./nav-bar.js";
 
-nav.insertNavBar();
+let pageName = window.location.pathname;
+console.log(pageName);
+if (pageName === '/'){
+    //Insert navbar for index page at element with id of "nav-el"
+    nav.insertIndexNavBar("nav-el");
+} else {
+    //Insert nabar for countries page at element with id of "nav-el"
+    nav.insertNavBar("nav-el");
+}
 createFooterText();
--- src/scripts/nav-bar.js
+++ src/scripts/nav-bar.js
@@ -1,7 +1,8 @@
-export const insertNavBar = () => {
-    const navEl = document.getElementById("nav-el");
+//This function should be called from countries page, otherwise pathing will not work
+export const insertNavBar = (elementId) => {
+    const elementLocation = document.getElementById(elementId);
 
-    navEl.innerHTML = `
+    elementLocation.innerHTML = `
     <ul class="nav__bar">
         <li class="nav__country">
             <a href="../index.html">
@@ -33,4 +34,40 @@ export const insertNavBar = () => {
             </a>
         </li>
     </ul>`;
+}
+
+
+//This function should only be called from index.html, otherwise the file pathing will be wrong
+export const insertIndexNavBar = (elementId) => {
+    const elementLocation = document.getElementById(elementId);
+
+    elementLocation.innerHTML = `
+        <ul class="nav__bar">
+            <li class="nav__country">
+                <a href="index.html">
+                    <img src="images/logo.png" alt="logo">
+                </a>
+            </li>
+            <li class="nav__country">
+                <a href="./countries/england.html">
+                    <img src="images/EnglishFlag.png" alt="english flag">
+                </a>
+            </li>
+            <li class="nav__country">
+                <a href="countries/japan.html">
+                    <img src="images/JapanFlag.png" alt="japanese flag">
+                </a>
+            </li>
+            <li class="nav__country">
+                <a href="countries/finland.html">
+                    <img src="images/FinlandFlag.png" alt="finnish flag">
+                </a>
+            </li>
+            <li class="nav__country">
+                <a href="countries/croatia.html">
+                    <img src="images/CroatiaFlag.png" alt="croatian flag">
+                </a>
+            </li>
+        </ul>
+    `;
 }
